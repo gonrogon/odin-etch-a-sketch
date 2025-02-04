6d883e914dd8a9b1be6e1cfc0549f37430a904f8
gonrogon <gonrogon@gmail.com>
2025-02-04 17:25:34 +0100

    Change the color when the mouse is over a cell

--- script.js
+++ script.js
@@ -8,10 +8,29 @@ function setupGrid(width, height)
         {
             let cell = document.createElement("div");
             cell.classList.add("cell");
-
             grid.appendChild(cell);
         }
     }
+
+    grid.addEventListener("mouseover", onMouseOver);
+    grid.addEventListener("mouseout",  onMouseOut);
+}
+
+function onMouseOver(evt)
+{
+    let cell = evt.target;
+    setColor(cell, "#f00");
+}
+
+function onMouseOut(evt)
+{
+    let cell = evt.target;
+    setColor(cell, "");
+}
+
+function setColor(cell, color)
+{
+    cell.style.backgroundColor = color;
 }
 
 setupGrid(16, 16);
