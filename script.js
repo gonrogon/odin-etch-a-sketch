723f024d277e8ac3ad72e0530dd6cc89c0a943cd
gonrogon <gonrogon@gmail.com>
2025-02-05 12:05:35 +0100

    Add a progressive darkening effect

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+
+
+function setup()
+{
+    let button = document.querySelector("#buttonSize");
+    button.addEventListener('click', e => {
+        let result = prompt('Enter the new size (max 100)', 16);
+        let number = parseInt(result);
+
+        if (isNaN(number) || number <= 0 || number > 100)
+        {
+            alert("Invalid size");
+            return;
+        }
+
+        setupGrid(number, number);
+    })
+    // Set up the initial grid.
+    setupGrid(16, 16);
+}
+
 function setupGrid(width, height)
 {
     let grid = document.querySelector("#grid");
@@ -20,35 +41,52 @@ function setupGrid(width, height)
     grid.addEventListener("mouseover", onMouseOver);
 }
 
-function setup()
-{
-    let button = document.querySelector("#buttonSize");
-    button.addEventListener('click', e => {
-        let result = prompt('Enter the new size (max 100)', 16);
-        let number = parseInt(result);
-
-        if (isNaN(number) || number <= 0 || number > 100)
-        {
-            alert("Invalid size");
-            return;
-        }
-
-        setupGrid(number, number);
-    })
-}
-
 function onMouseOver(evt)
 {
-    let r = Math.round(Math.random() * 255);
-    let g = Math.round(Math.random() * 255);
-    let b = Math.round(Math.random() * 255);
+    let cell = evt.target;
 
-    setColorRGB(evt.target, r, g, b);
+    if (hasColor(cell))
+    {
+        let opacity = getOpacity(cell);
+
+        if (opacity < 1)
+        {
+            setOpacity(cell, opacity + 0.1);
+        }
+    }
+    else
+    {
+        let r = Math.round(Math.random() * 255);
+        let g = Math.round(Math.random() * 255);
+        let b = Math.round(Math.random() * 255);
+
+        setOpacity (cell, 0.1);
+        setColorRGB(cell, r, g, b);
+    }
+}
+
+function getOpacity(cell) {
+    let opacity = cell.style.opacity;
+    let value   = 0;
+
+    if (opacity.length != 0)
+    {
+        value = parseFloat(cell.style.opacity);
+    }
+    
+    return value;
+}
+
+function setOpacity(cell, opacity) {
+    cell.style.opacity = Math.min(Math.max(opacity, 0), 1);
+}
+
+function hasColor(cell) {
+    return cell.style.backgroundColor.length != 0;
 }
 
 function setColorRGB(cell, r, g, b) {
     cell.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
 }
 
-setupGrid(16, 16);
 setup();
